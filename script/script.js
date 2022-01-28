19a8944b5e4ae55e13162528528a6d179b1fa2ce
Igor Jelic <igor.jelic.98@gmail.com>
2022-01-28 02:44:24 +0100

    added contact widget

--- script/script.js
+++ script/script.js
@@ -2,6 +2,13 @@ const terminalToggleBtn = document.querySelector(".terminal-toggle");
 const terminalContainer = document.querySelector(".terminal-container");
 const terminalInput = document.querySelector(".terminal-console textarea");
 
+const contactToggleBtn = document.querySelector(".contact-toggle");
+const socialNetworkDiv = document.querySelector(".social-network");
+
+contactToggleBtn.addEventListener("click", () => {
+  socialNetworkDiv.classList.toggle("active");
+});
+
 terminalToggleBtn.addEventListener("click", () => {
   terminalContainer.classList.toggle("hidden");
   terminalToggleBtn.classList.toggle("hidden");
