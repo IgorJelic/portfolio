46929a66336013f4dd28be21c5a2480ec2e46e0e
Igor Jelic <igor.jelic.98@gmail.com>
2022-02-03 01:18:21 +0100

    terminal navigation done!!!

--- script/script.js
+++ script/script.js
@@ -5,13 +5,35 @@ const terminalInput = document.getElementById("terminal");
 const contactToggleBtn = document.querySelector(".contact-toggle");
 const socialNetworkDiv = document.querySelector(".social-network");
 
+const contentContainer = document.querySelector(".content-container");
+const contentRight = document.querySelector(".content-right");
+
+// ! TERMINAL VARIABLES
 // CV path
 const pathCV = "../files/MyCV.pdf";
 
 let actionHistory = [];
 
+const DIRECTORIES = {
+  home: 0,
+  skills: 1,
+  projects: 2,
+  interests: 3,
+};
+
+// const AVAILABLE_DIRECTORIES = [
+//   "home",
+//   "my-skills",
+//   "my-projects",
+//   "my-interests",
+// ];
+
+let CURRENT_DIRECTORY = DIRECTORIES.home;
+
 const help =
-  "dir - show current directory path\ncd <path> - change directory to <path>\nopen cv - open MyCV.pdf in new tab\ndownload cv - download MyCV.pdf\nquit - close portfolio\nclose - close terminal\n";
+  "dir - show available directories\ncd <target> - change directory to <target>\nopen cv - open MyCV.pdf in new tab\ndownload cv - download MyCV.pdf\nquit - close portfolio\nclose - close terminal\n";
+
+// ! END OF TERMINAL VARIABLES
 
 contactToggleBtn.addEventListener("click", () => {
   socialNetworkDiv.classList.toggle("active");
@@ -27,11 +49,6 @@ terminalToggleBtn.addEventListener("click", () => {
     document.activeElement.blur();
   }
 });
-
-// todo Add shortcut for Terminal pop-up
-// window.addEventListener("keypress", (e) => {
-//   console.log(e.key);
-// });
 
 terminalInput.addEventListener("keydown", (e) => {
   console.log(e.key);
@@ -52,17 +69,32 @@ terminalInput.addEventListener("keydown", (e) => {
 function executeAction(action) {
   const formattedAction = action.replace(/ /g, "");
 
-  console.log(formattedAction.toLowerCase());
-  switch (formattedAction) {
+  const link = document.createElement("a");
+
+  switch (formattedAction.toLowerCase()) {
+    case "dir":
+      return "home\nskills\nprojects\ninterests\n";
+    case "cdhome":
+      slide(DIRECTORIES.home);
+      return "";
+    case "cdskills":
+      slide(DIRECTORIES.skills);
+      return "";
+    case "cdprojects":
+      slide(DIRECTORIES.projects);
+      return "";
+    case "cdinterests":
+      slide(DIRECTORIES.interests);
+      return "";
+
     case "opencv":
       window.open(pathCV, "_blank");
       return "";
     case "downloadcv":
-      const link = document.createElement("a");
       link.href = pathCV;
       link.download = "MyCv.pdf";
       link.dispatchEvent(new MouseEvent("click"));
-      link.remove();
+      // link.remove();
       return "";
     case "close":
       terminalToggleBtn.dispatchEvent(new Event("click"));
@@ -75,4 +107,14 @@ function executeAction(action) {
     default:
       return "";
   }
+}
+
+function slide(destination) {
+  const sliderHeight = contentContainer.clientHeight;
+
+  if (destination === CURRENT_DIRECTORY) return;
+  // let movement = CURRENT_DIRECTORY - destination;
+  contentRight.style.transform = `translateY(-${destination * sliderHeight}px)`;
+
+  CURRENT_DIRECTORY = destination;
 }
